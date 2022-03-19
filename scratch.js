375e8ccaa6e0f72dc886b810bd6cef6ed8b29b0d
jonathanko500 <jkko9010@gmail.com>
2022-03-18 21:22:10 -0700

    prac arr filter

--- scratch.js
+++ scratch.js
@@ -1,2 +1,28 @@
-let z = 'why';
-console.log(z);
+let unitArr = [
+    {
+        name: "HTTP",
+        startDate: '5-11',
+        active: true
+    },
+    {
+        name: "CSS",
+        startDate: '5-11',
+        active: true
+    },
+    {
+        name: "JS",
+        startDate: '5-11',
+        active: false
+    },
+    {
+        name: "HTML",
+        startDate: '5-11',
+        active: false
+    }
+]
+ 
+let activeUnit1 = unitArr.filter(unit => !unit.active);
+console.log(activeUnit1);
+
+let activeUnit2 = unitArr.filter(unit => unit.active);
+console.log(activeUnit2);
