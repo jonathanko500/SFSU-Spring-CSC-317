13a267cdbc3774d1fecb0d47d042d3c45e09dbf3
jonathanko500 <jkko9010@gmail.com>
2022-03-20 21:58:31 -0700

    more lecture stuff

--- scratch.js
+++ scratch.js
@@ -1,28 +1,8 @@
-let unitArr = [
-    {
-        name: "HTTP",
-        startDate: '5-11',
-        active: true
-    },
-    {
-        name: "CSS",
-        startDate: '5-11',
-        active: true
-    },
-    {
-        name: "JS",
-        startDate: '5-11',
-        active: false
-    },
-    {
-        name: "HTML",
-        startDate: '5-11',
-        active: false
-    }
-]
- 
-let activeUnit1 = unitArr.filter(unit => !unit.active);
-console.log(activeUnit1);
+const funcStuff = function()
+{
+    return 'stuff';
+}
 
-let activeUnit2 = unitArr.filter(unit => unit.active);
-console.log(activeUnit2);
+const arrowFunc = para => para + ' stuff';
+
+console.log(arrowFunc('what'));
